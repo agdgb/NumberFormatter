64cca2bbabb04940b5d510f4dab439745a024ecd
agdgb <137877737+agdgb@users.noreply.github.com>
2026-04-01 09:30:13 +0300

    Update ExampleProject to showcase NumberFormatter features

--- src/ExampleProject/Controllers/HomeController.cs
+++ src/ExampleProject/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using NumberFormatter.AspNetCore;
 
 namespace NumberFormatter.Demo.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly INumberFormatterService _formatterService;
+
+    public HomeController(INumberFormatterService formatterService)
+    {
+        _formatterService = formatterService;
+    }
+
     public IActionResult Index()
     {
+        ViewBag.FormattedValue = _formatterService.FormatShort(1234567.89m);
+        ViewBag.FormattedCurrency = _formatterService.FormatCurrency(9876543.21m, currencyCode: "EUR");
         return View();
     }
 }
